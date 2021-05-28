e82e26cd913a4fac6203287f843d8828c84480b0
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-05-28 13:13:38 +0100

    AEDB-36 - Add postrequest to send email reminder

---
+++ src/SFA.DAS.EmployerDemand.Jobs.UnitTests/Infrastructure/Api/Requests/WhenBuildingPostSendReminderEmailRequest.cs
@@ -0,0 +1,21 @@
+using System;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerDemand.Jobs.Infrastructure.Api.Requests;
+
+namespace SFA.DAS.EmployerDemand.Jobs.UnitTests.Infrastructure.Api.Requests
+{
+    public class WhenBuildingPostSendReminderEmailRequest
+    {
+        [Test, AutoData]
+        public void Then_The_Url_Is_Correctly_Constructed(Guid id, Guid courseDemandId)
+        {
+            //Act
+            var actual = new PostSendReminderEmailRequest(id, courseDemandId);
+            
+            //Assert
+            actual.PostUrl.Should().Be($"demand/{courseDemandId}/send-reminder-email/{id}");
+        }
+    }
+}
