d1588c2eff287367cd375af5d935025f71d1993f
Scott Wakefield <scott@expertcc.co.uk>
2021-06-24 11:35:55 +0100

    AEDB-81 added anonymise request.

---
+++ src/SFA.DAS.EmployerDemand.Jobs.UnitTests/Infrastructure/Api/Requests/WhenBuildingPostAnonymiseDemandRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerDemand.Jobs.Infrastructure.Api.Requests;
+
+namespace SFA.DAS.EmployerDemand.Jobs.UnitTests.Infrastructure.Api.Requests
+{
+    public class WhenBuildingPostAnonymiseDemandRequest
+    {
+        [Test, AutoData]
+        public void Then_The_Url_Is_Correctly_Constructed(Guid demandId)
+        {
+            //Act
+            var actual = new PostAnonymiseDemandRequest(demandId);
+            
+            //Assert
+            actual.PostUrl.Should().Be($"demand/{demandId}/anonymise");
+        }
+    }
+}
