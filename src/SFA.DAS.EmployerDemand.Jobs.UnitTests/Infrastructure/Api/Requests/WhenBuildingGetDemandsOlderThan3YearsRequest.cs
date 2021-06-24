d81b7a3656391dda26ee1a48b9538f92eba179fb
Scott Wakefield <scott@expertcc.co.uk>
2021-06-24 10:13:28 +0100

    AEDB-81 adding get request.

---
+++ src/SFA.DAS.EmployerDemand.Jobs.UnitTests/Infrastructure/Api/Requests/WhenBuildingGetDemandsOlderThan3YearsRequest.cs
@@ -0,0 +1,20 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerDemand.Jobs.Infrastructure.Api.Requests;
+
+namespace SFA.DAS.EmployerDemand.Jobs.UnitTests.Infrastructure.Api.Requests
+{
+    public class WhenBuildingGetDemandsOlderThan3YearsRequest
+    {
+        [Test]
+        public void Then_Url_Set_Correctly()
+        {
+            //Act
+            var actual = new GetDemandsOlderThan3YearsRequest();
+            
+            //Assert
+            actual.GetUrl.Should().Be("demand/older-than-3-years");
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerDemand.Jobs/Infrastructure/Api/Requests/GetDemandsOlderThan3YearsRequest.cs
@@ -0,0 +1,9 @@
+﻿using SFA.DAS.EmployerDemand.Jobs.Domain.Interfaces;
+
+namespace SFA.DAS.EmployerDemand.Jobs.Infrastructure.Api.Requests
+{
+    public class GetDemandsOlderThan3YearsRequest : IGetApiRequest
+    {
+        public string GetUrl => "demand/older-than-3-years";
+    }
+}
